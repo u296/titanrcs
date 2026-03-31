2eedbcba395595d3d266bfa18bcd0a60a04f5702
u296 <kerosene296@gmail.com>
2026-03-31 23:28:58 +0200

    initial but buggy fft usage

--- src/rcs/rcs_render.c
+++ src/rcs/rcs_render.c
@@ -2,6 +2,8 @@
 
 #include "common.h"
 #include "context.h"
+#include "vkFFT/vkFFT_AppManagement/vkFFT_RunApp.h"
+#include "vkFFT/vkFFT_Structs/vkFFT_Structs.h"
 #include <assert.h>
 
 void render_rcs_imgs(RenderContext* ctx) {
@@ -29,7 +31,8 @@ void render_rcs_imgs(RenderContext* ctx) {
     clear_depth.depthStencil.stencil = 0;
 
     rpbi.clearValueCount = 4;
-    rpbi.pClearValues = (VkClearValue[]){clearcol, clearcol, clearcol, clear_depth};
+    rpbi.pClearValues =
+        (VkClearValue[]){clearcol, clearcol, clearcol, clear_depth};
 
     VkViewport viewport = {};
     viewport.x = 0.0f;
@@ -51,13 +54,15 @@ void render_rcs_imgs(RenderContext* ctx) {
     vkBeginCommandBuffer(cmdbuf, &cbbi);
 
     vkCmdBeginRenderPass(cmdbuf, &rpbi, VK_SUBPASS_CONTENTS_INLINE);
-    vkCmdBindPipeline(cmdbuf, VK_PIPELINE_BIND_POINT_GRAPHICS, ctx->rcs_resources.pipeline);
+    vkCmdBindPipeline(cmdbuf, VK_PIPELINE_BIND_POINT_GRAPHICS,
+                      ctx->rcs_resources.pipeline);
 
     vkCmdBindVertexBuffers(cmdbuf, 0, 1, vbufs, vbuf_offsets);
-    vkCmdBindIndexBuffer(cmdbuf, ctx->rcs_resources.mesh.indexbuf.buf, 0, VK_INDEX_TYPE_UINT16);
+    vkCmdBindIndexBuffer(cmdbuf, ctx->rcs_resources.mesh.indexbuf.buf, 0,
+                         VK_INDEX_TYPE_UINT16);
     vkCmdBindDescriptorSets(cmdbuf, VK_PIPELINE_BIND_POINT_GRAPHICS,
-                            ctx->rcs_resources.pipeline_layout, 0, 1, &ctx->rcs_resources.descset,
-                            0, NULL);
+                            ctx->rcs_resources.pipeline_layout, 0, 1,
+                            &ctx->rcs_resources.descset, 0, NULL);
 
     vkCmdSetViewport(cmdbuf, 0, 1, &viewport);
     vkCmdSetScissor(cmdbuf, 0, 1, &scissor);
@@ -66,9 +71,8 @@ void render_rcs_imgs(RenderContext* ctx) {
     // vkCmdDraw(cmdbuf, 3, 1, 0, 0);
     vkCmdEndRenderPass(cmdbuf);
 
-    
-
     // TEMPORARY HACK TO BLIT TO SWAPCHAIN
+    // also allows for transfer to the fft buffer
 
     VkImageMemoryBarrier renderBarrier = {};
     renderBarrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
@@ -76,14 +80,103 @@ void render_rcs_imgs(RenderContext* ctx) {
     renderBarrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
     renderBarrier.srcAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
     renderBarrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-    renderBarrier.image = ctx->rcs_resources.rendtargets[0].img; // Your offscreen image
+    renderBarrier.image =
+        ctx->rcs_resources.rendtargets[0].img; // Your offscreen image
     renderBarrier.subresourceRange =
         (VkImageSubresourceRange){VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1};
 
     vkCmdPipelineBarrier(cmdbuf, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
-                         VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, NULL, 0, NULL, 1, &renderBarrier);
+                         VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, NULL, 0, NULL, 1,
+                         &renderBarrier);
 
     // END HACK
+
+    VkBuffer fft_buf = ctx->rcs_resources.fft_buf.buf;
+
+    VkBufferImageCopy reg = {};
+    reg.bufferOffset = 0;
+    reg.bufferRowLength = 0;
+    reg.bufferImageHeight = 0;
+
+    reg.imageSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    reg.imageSubresource.mipLevel = 0;
+    reg.imageSubresource.baseArrayLayer = 0;
+    reg.imageSubresource.layerCount = 1;
+
+    const u32 w = 256, h = 256; // should change this to a
+                                // more global solution
+
+    reg.imageOffset = (VkOffset3D){0, 0, 0};
+    reg.imageExtent = (VkExtent3D){w, h, 1};
+
+    vkCmdCopyImageToBuffer(cmdbuf, ctx->rcs_resources.rendtargets[0].img,
+                           VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, fft_buf, 1,
+                           &reg);
+
+    VkBufferMemoryBarrier bar_bufpostcp = {};
+    bar_bufpostcp.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+    bar_bufpostcp.buffer = fft_buf;
+    bar_bufpostcp.offset = 0;
+    bar_bufpostcp.size = VK_WHOLE_SIZE;
+    bar_bufpostcp.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    bar_bufpostcp.dstAccessMask =
+        VK_ACCESS_SHADER_WRITE_BIT | VK_ACCESS_SHADER_READ_BIT;
+    bar_bufpostcp.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    bar_bufpostcp.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+
+    vkCmdPipelineBarrier(cmdbuf, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                         VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0, NULL, 1,
+                         &bar_bufpostcp, 0, NULL);
+
+    VkFFTLaunchParams lp = {};
+    lp.commandBuffer = &cmdbuf;
+    lp.buffer = &fft_buf;
+
+    VkFFTAppend(ctx->backend.fft, -1, &lp);
+
+    VkBufferMemoryBarrier bar_bufpostfft = {};
+    bar_bufpostfft.sType = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+    bar_bufpostfft.buffer = fft_buf;
+    bar_bufpostfft.offset = 0;
+    bar_bufpostfft.size = VK_WHOLE_SIZE;
+    bar_bufpostfft.srcAccessMask = VK_ACCESS_SHADER_WRITE_BIT;
+    bar_bufpostfft.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+    bar_bufpostfft.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    bar_bufpostfft.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+
+    VkImageMemoryBarrier bar_imgpostfft = {};
+    bar_imgpostfft.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    bar_imgpostfft.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+    bar_imgpostfft.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+    bar_imgpostfft.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+    bar_imgpostfft.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    bar_imgpostfft.image =
+        ctx->rcs_resources.rendtargets[0].img; // Your offscreen image
+    bar_imgpostfft.subresourceRange =
+        (VkImageSubresourceRange){VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1};
+
+    vkCmdPipelineBarrier(cmdbuf, VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT,
+                         VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, NULL, 1,
+                         &bar_bufpostfft, 1, &bar_imgpostfft);
+
+    vkCmdCopyBufferToImage(cmdbuf, fft_buf,
+                           ctx->rcs_resources.rendtargets[0].img,
+                           VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &reg);
+
+    VkImageMemoryBarrier bar_imgpostfftcopy = {};
+    bar_imgpostfftcopy.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    bar_imgpostfftcopy.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+    bar_imgpostfftcopy.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+    bar_imgpostfftcopy.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    bar_imgpostfftcopy.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+    bar_imgpostfftcopy.image =
+        ctx->rcs_resources.rendtargets[0].img; // Your offscreen image
+    bar_imgpostfftcopy.subresourceRange =
+        (VkImageSubresourceRange){VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1};
+
+    vkCmdPipelineBarrier(cmdbuf, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                         VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, NULL, 0, NULL, 1,
+                         &bar_imgpostfftcopy);
 
     VkResult r = vkEndCommandBuffer(cmdbuf);
     assert(r == VK_SUCCESS);
@@ -95,7 +188,8 @@ void render_rcs_imgs(RenderContext* ctx) {
     si.pCommandBuffers = &cmdbuf;
     si.signalSemaphoreCount = 0;
 
-    r = vkQueueSubmit(ctx->backend.queues.graphics_queue, 1, &si, VK_NULL_HANDLE);
+    r = vkQueueSubmit(ctx->backend.queues.graphics_queue, 1, &si,
+                      VK_NULL_HANDLE);
     assert(r == VK_SUCCESS);
 
     vkQueueWaitIdle(ctx->backend.queues.graphics_queue);
