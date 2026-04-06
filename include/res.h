d646f353da9b08f4700b393b5cc313a4c2b429a4
u296 <kerosene296@gmail.com>
2026-04-06 20:21:40 +0200

    set resolution to 4K on mac

--- include/res.h
+++ include/res.h
@@ -3,7 +3,11 @@
 
 #include "common.h"
 
+#ifdef __APPLE__
+constexpr u32 RCS_RESOLUTION = 4096u;
+#else
 constexpr u32 RCS_RESOLUTION = 8192u;
+#endif
 constexpr bool WANT_VALIDATION = true;
 constexpr u32 N_MAX_INFLIGHT = 1;
 
