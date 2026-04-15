c653b24f688480c2999978c8b3d79ea18a4ea14a
u296 <=>
2026-04-15 16:25:58 +0200

    add ptd to comp

--- shaders/rcs/comp.sh
+++ shaders/rcs/comp.sh
@@ -1,1 +1,4 @@
-glslc frag1.frag -o frag.spv -O && glslc vert1.vert -o vert.spv -O && glslc reduction.comp -o reduction.spv -O
+glslc frag1.frag -o frag.spv -O &&\
+glslc vert1.vert -o vert.spv -O &&\
+glslc reduction.comp -o reduction.spv -O &&\
+glslc ptd_frag.frag -o ptd_frag.spv -O
