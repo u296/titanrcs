4294e00b83a4b1483fa5d78a46bb50bd9cc4291a
u296 <=>
2026-05-10 23:29:01 +0200

    change line width

--- include/res.h
+++ include/res.h
@@ -12,9 +12,16 @@ constexpr u64 RCS_CROPFRACTION =
     4; // if ~2 or less, output will be bugged near the edges
 constexpr f32 RCS_RANGE = 1000000.0f;
 constexpr f32 RCS_BOXSIZE = 20.0f;
-constexpr f32 RCS_LINEWIDTH = 1;//(f32)(RCS_RESOLUTION/256ull >= 4? RCS_RESOLUTION/256ull : 4); // this seems decent for high res
+constexpr f32 RCS_LINEWIDTH = 4;
 /*
-Setting the linewidth too high will cause part of the line to get hidden behind the geometry, this needs to be taken into consideration.
+Setting the linewidth too high will cause part of the line to get hidden behind
+the geometry, this needs to be taken into consideration.
+
+I just tried linewidths 1, 3, 4 and 32 and they all pretty much worked the same.
+They gave the same RCS, although 1 had a lot of bullshit in the far field. 32
+started to lag behind a little on the big peaks, but other than that they're
+identical, even down to the noise they get. Just be sure to keep the ribbon
+smaller than the wavelength.
 */
 constexpr bool WANT_VALIDATION = true;
 constexpr bool BUILD_SHARP_EDGES = true;
